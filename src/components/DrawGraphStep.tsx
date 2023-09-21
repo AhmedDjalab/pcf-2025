c8419bacf53e7e6e2e65984fa9032821d45099d9
Ahmed <ahmedlight@yandex.com>
2023-09-21 12:51:23 +0100

    export pdf bt style

--- src/components/DrawGraphStep.tsx
+++ src/components/DrawGraphStep.tsx
@@ -546,8 +546,12 @@ function DrawGraphStep() {
   return (
     <div className="flex flex-col">
       <div>
-        <button onClick={() => saveAsPdfOrImage("pdf")}>Save as PDF</button>
-        <button onClick={() => saveAsPdfOrImage("image")}>Save as Image</button>
+        <button
+          className="focus:outline-none mt-5 text-white bg-purple-500 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
+          onClick={() => saveAsPdfOrImage("pdf")}
+        >
+          Save as PDF
+        </button>
       </div>
       <div className="flex flex-col" id="graph-container">
         <div className="graph-container">
