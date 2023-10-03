9b341abbbb3b414450c84f2d9c42da2fa93e6619
Ahmed <ahmedlight@yandex.com>
2023-10-03 07:47:12 +0100

    Add Connect To us style For email

--- src/Auth/Login.tsx
+++ src/Auth/Login.tsx
@@ -203,8 +203,11 @@ function Login() {
               <div className="flex items-center justify-between space-x-2">
                 <span className="w-1/5 h-px bg-gray-300 dark:bg-gray-700"></span>
                 <p className="text-sm text-gray-500 dark:text-gray-400 text-center">
-                  Vous n’avez pas de compte ? contactez-nous:{" "}
-                  <span className="text-blue-700">admin@ientreprize.com</span>
+                  Vous n’avez pas de compte ?
+                  <p>
+                    contactez-nous:{" "}
+                    <p className="text-blue-700">admin@ientreprize.com</p>
+                  </p>
                 </p>
                 <span className="w-1/5 h-px bg-gray-300 dark:bg-gray-700"></span>
               </div>
