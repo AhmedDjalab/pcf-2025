c001a110b9402e5306d5d1a4926f714f764d3083
Ahmed <ahmedlight@yandex.com>
2023-10-17 14:38:17 +0100

    add suppreime to back i ntask slot

--- src/components/TaskSlotsStep.tsx
+++ src/components/TaskSlotsStep.tsx
@@ -174,7 +174,7 @@ const TaskSlotsList = ({ setCurrentStep, currentStep }: MultiStepFormProps) => {
           onClick={() => setCurrentStep(currentStep - 1)} // Handle going back to the previous step
           className=" text-white bg-gray-400 hover:bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-600 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
         >
-          {t("taskSlotsList.buttons.delete")}
+          {t("taskSlotsList.buttons.back")}
         </button>
         <button
           type="button"
